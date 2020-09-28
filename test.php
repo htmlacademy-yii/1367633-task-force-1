8feeb1d7fbd50fec4a8f15a6f2eb346293fc3dbe
DeathSpirit <nazaryuzhyn@gmail.com>
2020-09-28 10:04:34 +0300

    ✌️

--- test.php
+++ test.php
@@ -1,3 +1,19 @@
 <?php
 
 	require_once __DIR__ . '/vendor/autoload.php';
+
+	use TaskForce\models\Task;
+
+	$task = new Task(1, 2);
+
+	echo '<pre>';
+
+	print_r($task::STATUS_NAMES);
+
+	print_r($task::ACTION_NAMES);
+
+	print_r($task->getNextStatus("done"));
+
+	assert($task->getNextStatus("done") == Task::STATUS_PERFORMED, 'Ожидайте действие: "done"');
+	assert($task->getNextStatus("cancel") == Task::STATUS_CANCELED, 'Ожидайте действие: "cancel"');
+	
