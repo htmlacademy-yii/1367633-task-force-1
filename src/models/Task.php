c2193a7287b2174f0fd855b15debac8348381e08
DeathSpirit <nazaryuzhyn@gmail.com>
2020-10-09 18:10:12 +0300

    ✌️

--- src/models/Task.php
+++ src/models/Task.php
@@ -53,8 +53,7 @@
 
 		public function setStatus($status)
 		{
-			if(!array_key_exists($status, self::STATUS_NAMES))
-			{
+			if(!array_key_exists($status, self::STATUS_NAMES)){
 				return false;
 			}
 
@@ -64,8 +63,7 @@
 
 		public function getActions($idUser)
 		{
-			if(!$this->status)
-			{
+			if(!$this->status){
 				return null;
 			}
 
