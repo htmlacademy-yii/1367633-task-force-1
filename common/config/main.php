a1007cbe066b38739c218516a0311f5f6fc52879
DeathSpirit <nazaryuzhyn@gmail.com>
2020-11-24 21:00:46 +0200

    Избирательное поведение

--- common/config/main.php
+++ common/config/main.php
@@ -1,13 +1,24 @@
 <?php
 return [
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-    ],
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-    ],
+	'language' => 'ru-RU',
+	'aliases' => [
+		'@bower' => '@vendor/bower-asset',
+		'@npm'   => '@vendor/npm-asset',
+	],
+	'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+	'components' => [
+		'urlManager' => [
+			'enablePrettyUrl' => true,
+			'showScriptName' => false,
+			'enableStrictParsing' => false,
+			'rules' => [
+				'//' => '/',
+				'tasks' => 'tasks/index',
+				'users' => 'users/index',
+			],
+		],
+		'cache' => [
+			'class' => 'yii\caching\FileCache',
+		],
+	],
 ];
